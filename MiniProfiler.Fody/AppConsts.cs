9d5434cde45b05e3f91564c689f3223ade62c4b4
JeffDu <dushouke@qq.com>
2017-05-26 00:17:10 +0800

    consts

--- MiniProfiler.Fody/AppConsts.cs
+++ MiniProfiler.Fody/AppConsts.cs
@@ -2,6 +2,7 @@
     public class AppConsts {
         public static readonly string ProfilerOnAttributeName = "ProfilerAttributes.ProfilerOnAttribute";
         public static readonly string NoProfilerAttributeName = "ProfilerAttributes.NoProfilerAttribute";
+
         public static readonly string MiniProfilerName = "MiniProfiler";
     }
 }
